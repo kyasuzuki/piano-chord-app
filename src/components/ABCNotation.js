d48317112605d5b930bfad69627aef99ea380d01
Kya Suzuki <kyasuzuki@gmail.com>
2020-03-31 11:16:30 -0500

    added abc music notation

---
+++ src/components/ABCNotation.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import abcjs from "abcjs";
+import { Notation } from "react-abc";
+
+const notesABCArray = [
+  { abc: "C", midi: 48 },
+  { abc: "_D", midi: 49 },
+  { abc: "D", midi: 50 },
+  { abc: "_E", midi: 51 },
+  { abc: "E", midi: 52 },
+  { abc: "F", midi: 53 },
+  { abc: "_G", midi: 54 },
+  { abc: "G", midi: 55 },
+  { abc: "_A", midi: 56 },
+  { abc: "A", midi: 57 },
+  { abc: "_B", midi: 58 },
+  { abc: "B", midi: 59 },
+  { abc: "c", midi: 60 },
+  { abc: "_d", midi: 61 },
+  { abc: "d", midi: 62 },
+  { abc: "_e", midi: 63 },
+  { abc: "e", midi: 64 },
+  { abc: "f", midi: 65 },
+  { abc: "_g", midi: 66 },
+  { abc: "g", midi: 67 },
+  { abc: "_a", midi: 68 },
+  { abc: "a", midi: 69 },
+  { abc: "_b", midi: 70 },
+  { abc: "b", midi: 71 },
+  { abc: "c'", midi: 72 },
+  { abc: "_d'", midi: 73 },
+  { abc: "d'", midi: 74 },
+  { abc: "_e'", midi: 75 },
+  { abc: "e'", midi: 76 },
+  { abc: "f'", midi: 77 },
+  { abc: "_g'", midi: 78 },
+  { abc: "g'", midi: 79 },
+  { abc: "_a'", midi: 80 },
+  { abc: "a'", midi: 81 },
+  { abc: "_b'", midi: 82 },
+  { abc: "b'", midi: 83 }
+];
+
+export const ABCNotation = ({ chordNotes }) => {
+  const [chordNotesAbc, setChordNotesAbc] = useState("");
+  console.log(chordNotes);
+
+  useEffect(() => {
+    let fullABCString = "";
+    for (var i = 0; i < chordNotes.length; i++) {
+      for (var j = 0; j < notesABCArray.length; j++) {
+        //console.log("chordNotetMidi", chordNotes[i].midi);
+        //console.log("notesABCArrayMidi", notesABCArray[j].midi);
+        //console.log("chordNotesABC", chordNotesABC);
+        if (chordNotes[i].midi === notesABCArray[j].midi) {
+          //   let ABCNote = notesABCArray[j].abc;
+          //   console.log("ABCNote", ABCNote);
+          //setChordNotesABC([...chordNotesABC, notesABCArray[j].abc]);
+          let ABCString = notesABCArray[j].abc;
+          console.log(ABCString);
+          fullABCString += ABCString;
+          console.log(fullABCString);
+
+          console.log("chordNotesAbc", chordNotesAbc);
+        }
+      }
+    }
+    setChordNotesAbc(fullABCString);
+    console.log(chordNotesAbc);
+  }, [chordNotes]);
+
+  //const notation = "c'";
+  //   return chordNotesABC.map(chordNoteABC => (
+  //     <Notation notation={chordNoteABC} />
+  //   ));
+  // chordNotesABC.join("")
+  return <Notation notation={chordNotesAbc} />;
+};
