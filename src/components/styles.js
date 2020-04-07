58c18fcde49ea9bdf2905314ccf76255c6a4c547
Kya Suzuki <kyasuzuki@gmail.com>
2020-04-07 11:57:44 -0500

    added Grommet

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,35 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
-export const Div = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-`;
+// export const Grid = styled.div``;
 
-export const Spacer = styled.div`
-  padding: 20px;
-`;
+// export const Row = styled.div`
+//   display: flex;
+// `;
+
+// export const Col = styled.div`
+//   flex: ${props => props.siz};
+// `;
+
+// export const Div = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   height: 100vh;
+//   width: 100vw;
+// `;
+
+// export const Spacer = styled.div`
+//   padding: 20px;
+// `;
 
 // Select Key + Chord
-export const Form = styled.div`
-  display: flex;
-  flex-flow: column wrap;
-`;
+// export const Form = styled.div`
+//   display: flex;
+//   flex-flow: column wrap;
+// `;
 
-export const Select = styled.select`
-  width: 100%;
-  height: 100%;
-  background: white;
-  color: gray;
-  font-size: 20px;
-  border: none;
-  display: flex;
-`;
+// export const Select = styled.select`
+//   width: 100%;
+//   height: 100%;
+//   background: white;
+//   color: gray;
+//   font-size: 20px;
+//   border: none;
+//   display: flex;
+// `;
 
 // Chord Note Label
-export const NotesLabel = styled.label`
-  letter-spacing: 50px;
-`;
+// export const NotesLabel = styled.label`
+//   letter-spacing: 50px;
+// `;
